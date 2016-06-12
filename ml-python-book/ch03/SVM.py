259981be166c2a4a9e246b2fe8c1883988174273
Vishal Sharma <vishal2033@gmail.com>
2016-06-12 17:08:06 +0530

    sklearn SVM

---
+++ ml-python-book/ch03/SVM.py
@@ -0,0 +1,29 @@
+from sklearn import datasets
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+def run():
+	iris = datasets.load_iris()
+	X = iris.data[:,[2,3]]
+	y = iris.target
+	
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
+	
+	sc = StandardScaler();
+	sc.fit(X_train)
+	
+	X_train_std = sc.transform(X_train)
+	X_test_std  = sc.transform(X_test)
+
+	svm = SVC(C=1.0, kernel='linear', random_state=0)
+	svm.fit(X_train_std, y_train)
+
+	y_pred = svm.predict(X_test_std)
+
+	print ('Missclassified samples = %d' % (y_test != y_pred).sum())
+	print('Accuracy = %.2f' % accuracy_score(y_test, y_pred))
+
+	print(y_pred)
--- ml-python-book/ch03/zrun.py
+++ ml-python-book/ch03/zrun.py
@@ -1,6 +1,8 @@
 import SciPerceptron as sciPpn
 import LogisticRegression as logReg
+import SVM as svm
 
 def run():
 	#sciPpn.run()
-	logReg.run()
+	#logReg.run()
+	svm.run()
